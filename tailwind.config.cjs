a224ec1babd852cbdc79f3c8604a61f672249e5b
Gion Rubitschung INI-EOS-OEN <gion.rubitschung@swisscom.com>
2023-07-31 11:06:33 +0200

    feat: added skeleton ui with tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		import('path').join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		...import('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
+	]
+}
