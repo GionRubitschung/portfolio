66985bde414880c446513dd2d9731f2b55a60e0c
Gion Rubitschung INI-EOS-OEN <gion.rubitschung@swisscom.com>
2023-12-22 10:05:18 +0100

    feat: working on work page

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,7 +1,36 @@
 <script lang="ts">
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
 	import { setContext } from 'svelte';
+	import scLogo from '$lib/assets/sc_logo.svg?raw';
+	import Icon from '@iconify/svelte';
+
+	const workExpierences = [
+		{
+			title: 'DevOps Engineer',
+			description: 'mogi',
+			icon: scLogo,
+		},
+	];
 
 	setContext('pageTitle', 'My Experience');
 </script>
 
-<h1>Work Experience</h1>
+<h1 class="text-primary text-4xl">Professional Experience</h1>
+
+<p class="text-token">asdf</p>
+
+<Accordion>
+	{#each workExpierences as work}
+		<AccordionItem>
+			<svelte:fragment slot="lead">
+				<div class="w-8">
+					{@html work.icon}
+				</div></svelte:fragment
+			>
+			<svelte:fragment slot="summary">{work.title}</svelte:fragment>
+			<svelte:fragment slot="content">{work.description}</svelte:fragment>
+		</AccordionItem>
+	{/each}
+</Accordion>
+
+<h1 class="text-primary text-4xl">Academic Background</h1>
